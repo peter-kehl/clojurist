9b14275afd8648c1cbbaae28571df0e9667bdd52
Peter Kehl <peter.kehl@gmail.com>
2018-10-04 05:51:51 -0700

    dbg framework

--- src/clojurist/4Clojure05.clj
+++ src/clojurist/4Clojure05.clj
@@ -49,30 +49,102 @@
       (fn [z] (some #(pos? (mod z %)) xs))
       (iterate #(+ x %) x))))
 
-; insert `trace "description"` before function calls, like
-; (trace "+ on numbers" + 1 2)
-; Unfortunately, that disables compile time validation of number of parameters, so if functions/calls change,
-; to validate them remove `trace "message", compile, re-add trace "message" back if need be.
-; Beware when tracing errors: (for) creates a lazy sequence, hence callbacks will be delayed
-;(debug "adding" (+ 1 1))
-(defmacro debug [msg body]
+(defmacro practice-small-debug-macro [msg body]
       `(let [val# ~body]
          (println "DEBUG (MSG): " ~msg)
          (println "DEBUG (RET): " val#)
          val#))
 
-(defn debug [msg & expression-parts]
-  `((print "Calling" name "with ")   
-    (clojure.pprint/pprint args)
-    (try
-      (let [res (apply f args)]
-        (print name "returning ")
-        (clojure.pprint/pprint res)
-        res)
-      (catch Throwable e
-        (print "Threw" e)
-        (throw e)))))
+(defmacro practice-use-form [s]
+  (let [f (str &form)]
+    `(let [s# ~s] (str s# ~f s#))))
+; insert `trace "description"` before function calls, like
+; (trace "+ on numbers" + 1 2)
+; Unfortunately, that disables compile time validation of number of parameters, so if functions/calls change,
+; to validate them remove `trace "message", compile, re-add trace "message" back if need be.
+; Beware when tracing errors: (for) creates a lazy sequence, hence callbacks will be delayed
+(defn trace [msg f & args]
+  ((print "Calling" msg "with ")   
+   (clojure.pprint/pprint args)
+   (try
+     (let [res (apply f args)]
+       (print msg "returning ")
+       (clojure.pprint/pprint res)
+       res)
+     (catch Throwable e
+       (print msg "threw" e)
+       (throw e)))))
+ 
+
+;BIG TODO: wrap everythin in with-out-str somehow, so it indents user's calls to print.
+;TODO (time) - optional?
+(def dbg-indent-level (atom 0))
+(defn dbg>> [] (swap! dbg-indent-level #(inc %)))
+(defn dbg<< [] (swap! dbg-indent-level #(Math/max (dec %) 0))) ;TODO warn on negative?
+(defn dbg-indentation [] (repeat @dbg-indent-level "  "))
+(defn dbg-format [str]
+  (clojure.string/replace str "\n"#"\r?\n" (str (newline) (dbg-indentation)))) ;not using (newline) for the pattern, so that hard-coded new line character(s) work cross-platform.
+;alternatively: (binding [*out* ...] (callback...)) or (def *out* ....)
+(defn dbg-print [& args]
+  (apply print (map dbg-format args)))
+(defn dbg-println [& args]
+  (apply dbg-print args)
+  (println))
+(defn dbg-pprint [obj]
+  (let [unindented (with-out-str (clojure.pprint/pprint obj))
+        indented (dbg-format unindented)]
+    (print indented)))
+
+(defn dbg-call [msg fun & args]
+  (dbg-println "Calling:" msg "with args: ")
+  (dbg-pprint args)
+  (dbg>>)
+  (try
+    (let [res (apply fun args)]
+      (dbg<<)
+      (dbg-println "Returning from:" msg "value:")
+      (dbg-pprint res)
+      res)
+    (catch Throwable e
+      (dbg<<)
+      (dbg-print msg "Throwing from:" msg "throwable:" e)
+      (throw e))))
     
+
+(defmacro dbg [msgOrFun & others]
+  (let [msg (if (string? msgOrFun)
+              msgOrFun
+              (str &form)) ;without (str) the macro would inject the user's code unqouted
+        nilOrFun (if (string? msgOrFun)
+                   nil
+                   msgOrFun)
+        fun (if nilOrFun
+              nilOrFun
+              (first others))
+        args (if nilOrFun
+               others
+               (rest others))
+        fun-expr (if (not (symbol? fun))
+                   (if nilOrFun
+                     (str (nth &form 1))
+                     (str (nth &form 2))))
+        fun-holder (gensym 'fun-holder)]
+    (if true 
+      (list 'do ; need (do) to honour any enclosing branches of e.g. (if)
+        (if (not (symbol? fun))
+          (list 'dbg-println "Evaluating a fn expression to call:" fun-expr "for:" msg))
+        ;no need to pre-eval the function expression to call, because that is done as a part of calling dbg-call.
+        (list 'let `[~fun-holder ~fun]
+          (list 'dbg-println "Evaluating any args for:" msg)
+          (seq (apply conj ['dbg-call msg fun-holder] args)))))))
+  
+;TODO alternative (list 'dbg-call msg nilOrFun others)
+
+;replacement for skipping the dbg, but only for forms with a string message: 
+(defmacro dbg [& args] (rest &form))
+
+;macroexpand wants a quoted expression:
+;(macroexpand '(dbg ...))
 
 ;http://www.4clojure.com/problem/101 Levenshtein Distance
 ;alternating wide and deep:
@@ -82,67 +154,71 @@
   (fn levensh [from to]
     (letfn
       [(diff [cand] ;number of differences in naive comparison char by char (as if we allowed char replacements only)
-         (+
-            (apply + ;good that (+) returns 0
-              (map
-                #(if (= %1 %2) 1 0)
-                cand to))
-            (int (Math/abs (- (count to) (count cand))))))
+         (dbg "+" +
+           (apply + ;good that (+) returns 0
+             (map
+               #(if (= %1 %2) 1 0)
+               cand to))
+           (int (Math/abs (- (count to) (count cand))))))
        
-       ;generate a set of candidates with one-step changes
+       ;generate a seq of [candidate num-of-changes] with one-step changes ahead
        (next-candidates [[prev-candidate prev-num-of-changes]]
-         (let [prefix (apply str (for [pair-of-chars (map vector prev-candidate to) ;prefix is a shared initial part of: prev and to
-                                       :while (= (first pair-of-chars) (second pair-of-chars))]
-                                   (trace "first pair-of-chars" first pair-of-chars)))]
+         (let [prefix (dbg "apply str" apply str (for [pair-of-chars (map vector prev-candidate to) ;prefix is a shared initial part of: prev and to
+                                                       :while (= (first pair-of-chars) (second pair-of-chars))]
+                                                   (dbg "first pair-of-chars" first pair-of-chars)))]
            #_TODO-for-end-of-prefix-onwards_change-each-index
            #_TODO-merge-two-let
-           (println "Prefix " prefix)
+           (dbg-println "Prefix " prefix)
            (let [prev-count (count prev-candidate)
                  to-count (count to)
                  prefix-count (count prefix)]
-             (trace (str "str -> into prefix" prefix) into
+             (dbg "str -> into [] with prefix" into
                ; if a change reverts a previous change, we still count both changes. Such paths get eliminated by rating.
-               (into [] ;merge 1 or 2 out of the following 3 candidate possibilities
+               (dbg "into" into '() ;merge 1 or 2 out of the following 3 candidate possibilities
                  (if (<= prev-count to-count)
                    [[(str prefix
-                       (trace "get inc prefix-count1" get to (inc prefix-count)) ;added 1 char
+                       (dbg "get inc prefix-count1" get to (inc prefix-count)) ;added 1 char
                        (apply str (drop      prefix-count  prev-candidate)))
                      (inc prev-num-of-changes)] ;keep the rest
                     [(str prefix
-                       (trace "get inc prefix-count2" get to (inc prefix-count)) ;replaced 1 char
-                       (apply str (drop (inc prefix-count) prev-candidate)))]
-                    (inc prev-num-of-changes)] ;adjust the rest by 1 char
+                       (dbg "get inc prefix-count2" get to (inc prefix-count)) ;replaced 1 char
+                       (apply str (drop (inc prefix-count) prev-candidate)))
+                     (inc prev-num-of-changes)]] ;adjust the rest by 1 char
                    []))
                (if (>= prev-count to-count)
                  [[(str prefix
-                     (trace "into prefix 3: apply str" apply str (drop (inc prefix-count) prev-candidate)))]
-                  (inc prev-num-of-changes)] ;removed
+                     (dbg "into prefix 3: apply str" apply str (drop (inc prefix-count) prev-candidate)))
+                   (inc prev-num-of-changes)]] ;removed
                  [])))))
        
        ;toolkit on pairs/colls of [candidate num-of-changes]
        (compare-ignoring-effort [[cand1 _] [cand2 _]]
          (compare cand1 cand2))
        
+       ; merge candidate pairs from one and two, ignoring the effort. Used when merging backlog to priority, so that every possibility gets a chance.
+       ;TODO why isn't this called at all?
        (into-best-candidates [one two]
          (let [one-comparable (apply
                                 sorted-set-by
                                 compare-ignoring-effort
                                 one)]
-           (trace "into-best-candidates-> into" into
+           (dbg "into-best-candidates-> into" into
              (into #{}
                (for [from-two two]
                  (let [from-one (one-comparable from-two)]
-                   (if (or (nil? from-one) (> (second from-one) (second from-two))) ;if two  candidates, take that with fewer changes
+                   (if (or
+                           (nil? from-one)
+                           (> (second from-one) (second from-two))) ;if two  candidates, take that with fewer changes
                      from-two
                      from-one))))
              one))) ;add any missing items from one, but don't replace existing items (which were already compared between both one and two) 
        
        (rate [[candidate num-changes]]
-         (trace "+" + (diff candidate)
+         (dbg "+" + (diff candidate)
            num-changes))
        
        (compare-full [one two]
-         (trace "compare-full"
+         (dbg "compare-full"
            (fn [[cand1 num-ch1 :as pair1]
                 [cand2 num-ch2 :as pair2]]
              (let [likeness-and-effort (comp (rate pair1) (rate pair2))]
@@ -153,7 +229,7 @@
        
        (next-generation [prev-gen] ; Parameter and result are of type: coll of [candidate num-of-changes]
          (for [p prev-gen
-               n (trace "next-candidates" next-candidates p)
+               n (dbg "next-candidates" next-candidates p)
                c n]
            c))]
       ;if slow, transform somehow into a lazy seq.
@@ -177,12 +253,12 @@
           (second (first priority))
           (if (and (seq backlog) (< (/ (count priority) (count backlog) 0.05)))
             (recur (into priority backlog) (empty backlog) best-num-changes)
-            (let [priority-moved (trace "into->priority-moved" into (trace "empty priority" empty priority)
-                                   (trace "next-generation priority" next-generation priority))
+            (let [priority-moved (dbg "into->priority-moved" into (trace "empty priority" empty priority)
+                                   (dbg "next-generation priority" next-generation priority))
                   priority-moved-results (filter (fn [cand _] (= cand to)) priority-moved)
                   ;priority-moved-results-nums (map (fn [_ num-changes] num-changes))
                   priority-moved-best-num-changes (if (seq priority-moved-results)
-                                                    (apply min (map (fn [_ num-changes] num-changes)))
+                                                    (apply min (map (fn [_ num-changes] num-changes) priority-moved-results))
                                                     nil)
                   best-num (if best-num-changes
                              (if priority-moved-best-num-changes
@@ -204,7 +280,7 @@
                     backlog-next  (into backlog-keep (drop priority-next-count priority-keep))]
                 
                 (recur priority-next backlog-next best-num)))))))))
-(if true
+(if false
   (leven "kitten" "sitting"))
 ;get for ordered set/map returns an *existing* entry, not the given key:
 (assert (= ((sorted-set-by #(compare (mod % 3) (mod %2 3)) 1 2 4 5) 4) 1))
