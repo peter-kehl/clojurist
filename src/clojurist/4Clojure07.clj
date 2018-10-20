8600c29f7895e1ea7f54f9e9b6e850b85e25b7bd
Peter Kehl <peter.kehl@gmail.com>
2018-10-19 22:23:10 -0700

    #178 Poker best hand

--- src/clojurist/4Clojure07.clj
+++ src/clojurist/4Clojure07.clj
@@ -200,56 +200,84 @@
   (fn [codes]
     (let [code2struc (fn [[suit rank]]
                        {:suit ({\S :spade \H :heart \D :diamond \C :club} suit)
+                        ;Rank: 2..9, 10 ("T"), Jack, Queen, King, and Ace -> here 0..12
                         :rank (let [ascii (int rank)]
                                 (if (<= ascii (int \9))
                                   (- ascii (int \2))
                                   ({\T 8 \J 9 \Q 10 \K 11 \A 12} rank)))})
           strucs (map code2struc codes)
+          _ (println :strucs strucs)
           
-          suits (map :suit strucs)
-          ranks (map :rank strucs)
+          suits (#_dbgf map :suit strucs)
+          ranks (#_dbgf map :rank strucs)
+          _ (println :ranks ranks)
           
           same-rank (apply = suits)
-          standard-sequence (= (- (max ranks) (min ranks))
-                               4)
-          by-rank-inv (into
-                        (sorted-map-by
-                          (comp - count)) ;most frequent rank first; can't use (reverse ...) as that turns a map into a sequence - bad for (vals ...) below
-                        (clojure.set/map-invert
-                          (group-by :rank strucs)))]
+          each-rank-unique (= (count (into #{} ranks)) 5) ;each rank unique; with new CLJ could use (dedupe ..)
+          
+          standard-sequence (and each-rank-unique
+                                 (= (- (apply max ranks) (apply min ranks))
+                                    4))
+          _ (println :standard-sequence standard-sequence)
+          #_by-rank-rev-inv #_(into (sorted-map-by #(> (count %) (count %2))) (clojure.set/map-invert (group-by :rank strucs)))
+          seq-by-rank-rev-inv (sort-by
+                                (comp - count first) ;most frequent rank first; can't use sort-by neither reverse, as those turn a map into a sequence - bad for (vals ...) below
+                                (clojure.set/map-invert
+                                  (group-by :rank strucs)))
+          _ (println :by-rank-rev-inv seq-by-rank-rev-inv)
+          nth-frequent-rank-occurrence (fn [pos]
+                                         (#_dbgf #_"count nth seq" count (key (nth seq-by-rank-rev-inv pos))))]
+      ;three-of-a-kind (= (nth-frequent-rank-occurrence 0) 3)]
       (cond
         (and same-rank standard-sequence)
         :straight-flush
         
-        (some (fn [[_ cards]]
-                (= (count cards) 4))
-          by-rank)
+        #_(some (fn [[_ cards]] (= (count cards) 4)) by-rank)
+        (= (nth-frequent-rank-occurrence 0) 4)
         :four-of-a-kind
-      
-        (and (= (count by-rank-inv) 2)
-             (= (count (first (sort-by count (vals by-rank) 2))))) ;the other group must have 3 cards, since we have 2 groups only
+        
+        (and (= (count seq-by-rank-rev-inv) 2)
+             (= (nth-frequent-rank-occurrence 0) 3)) ;the other group must have 2 cards, since we have 2 groups only
         :full-house
         
         same-rank
         :flush
         
         (or standard-sequence
-            (let [non-aces (filter #(not= 12 %) ranks)]
+            (let [non-aces (filter #(not= 12 %) ranks)
+                  _ (println :non-aces non-aces)]
               (and (= (count non-aces) 4)
-                   (= (- (max non-aces) (min non-aces))
-                      3))))
+                   each-rank-unique
+                   (= (apply min non-aces) 0)
+                   (= (apply max non-aces) 3))))
+        
         :straight
         
-        
+        (= (nth-frequent-rank-occurrence 0) 3)
         :three-of-a-kind
         
+        (= (nth-frequent-rank-occurrence 0)
+           (nth-frequent-rank-occurrence 1)
+           2)
         :two-pair
         
+        (= (nth-frequent-rank-occurrence 0) 2)
         :pair
         
         :else
         :high-card))))
       
+
+(best-hand ["HA" "D2" "H3" "C9" "DJ"])
+(best-hand ["HA" "HQ" "SJ" "DA" "HT"])
+(best-hand ["HA" "DA" "HQ" "SQ" "HT"])
+(best-hand ["HA" "DA" "CA" "HJ" "HT"]) ;-> :three-of-a-kind
+(best-hand ["HA" "DK" "HQ" "HJ" "HT"]) ; straight
+(best-hand ["HA" "H2" "S3" "D4" "C5"]) ; straight
+(best-hand ["HA" "HK" "H2" "H4" "HT"])
+(best-hand ["HA" "DA" "CA" "HJ" "DJ"])
+(best-hand ["HA" "DA" "CA" "SA" "DJ"])
+(best-hand ["HA" "HK" "HQ" "HJ" "HT"])
 
 
 ;http://www.4clojure.com/problem/130 tree reparent
