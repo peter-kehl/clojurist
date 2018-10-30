d3b20d64a37d3541c2ba0488fc7ef8571b3ff0ae
Peter Kehl <peter.kehl@gmail.com>
2018-10-30 00:08:27 -0700

    Progress

--- src/clojurist/InProgress/140-bool-algebra.clj
+++ src/clojurist/InProgress/140-bool-algebra.clj
@@ -7,7 +7,7 @@
     ; map of maps {field: boolean}. Because of false, use contains? or nil? to detect relevance (presence) of a field.
     (let [upper-case? (fn [sym] (let [st (str sym)]
                                   (= (clojure.string/upper-case st) st)))
-          full-maps (into #{} ;a set of maps, each {upper-case-symbol boolean} for all symbols
+          full-maps (into #{} ;a set of maps, each {upper-case-symbol boolean...} for all symbols
                       (map
                         (fn [set-orig]
                           (into {} (map
@@ -17,15 +17,18 @@
                                      set-orig)))
                         sets-orig))
           fields (map ;a seq. of upper-case strings of all possible symbols. Easy, because
-                      ;all sets in sets-orig contain all symbols (either on/off - uppercase/lowercase).
+                      ;every set in sets-orig contains all symbols (either on/off - uppercase/lowercase).
                    #(clojure.string/upper-case %)
                    (first sets-orig))
           field-value-others (reduce ;3D map { upper-case { boolean-for-that-symbol {other-uppercase boolean...}}}
                                (fn [res full]
-                                 ;iterate over fields
+                                 ;TODO iterate over fields
                                  (assoc-in res (list (clojure.string/upper-case))))
                                #{}
-                               full-maps)]
+                               full-maps)
+          ;Where (= ((field-value-others chosen-upper-case) true) ((field-value-others chosen-upper-case) false), we can
+          ;simplify that rule by eliminating that chosen-upper-case from all connected rules 
+          _ 1]
       1)))    
       
     
--- src/clojurist/InProgress/195-permutate-depending-parenthesis.clj
+++ src/clojurist/InProgress/195-permutate-depending-parenthesis.clj
@@ -57,11 +57,116 @@
 ; 4. That gives you a result. Repeat for the next result.
 ; Optimisations:
 ; 1. Collect, pass & skip the number of consecutive closers on the very right.
-; 2. Change from parens-flat to parens-flatter: Move applying rightmost consecutive closers & openers into separate loop. That eliminates a complex conditional.
-; 3. NOT IMPLEMENTED: Change from parens-flat to parens-flatter: Move code that generates consecutive the bits part of new rightmost consecutive openers & closers to the first loop.  
+; 3. NOT IMPLEMENTED: Change from parens-flat to parens-flatish: Move code that generates
+; new rightmost consecutive openers & closers to the first loop. Why? Time results from parens-flatter show that splitting
+; loops adds much extra overhead. Hence merge loops together.
 ; Replacing (= XYZ abc) with (zero? abc) and reordering the iteration for it saves little: 605-680ms => 460-660ms
 ; clj -e "(time (count (filter #(zero? %) (interleave (repeat 1000000 1) (repeat 0)))))"
 
+(def parens-flatish
+  ; - prev: the last result
+  ; binary 1 for an opener (, 0 for a closer )
+  ; Return a seq. of complete solutions as numbers that in binary are of full-length (2x n). No need to return as a set
+  ; because the results are already unique.
+  ; For n-pairs=12: 620-740ms. (map humanise ...) takes only 40-120ms out of the total.
+  ; Added cons-closers => total 480ms. Turned off one last assert => total 460ms.
+  (fn [n-pairs]
+    (let [n-pairs*2 (* 2 n-pairs)
+          n-pairs*2-1 (dec n-pairs*2)]
+      (letfn [(digits [numb]
+                ;{:pre [(number? numb)]}
+                (vec (reverse (for [i (range 0 n-pairs*2)]
+                                (bit-test numb i)))))
+              (count-digits [numb digit]
+                ;{:pre [(number? numb) (or (= true digit) (= false digit) #_no-boolean?-in-CLJ-1_4)]}
+                (count (filter (partial = digit) (digits numb))))
+              
+              ; cons-closers is a group of bits - but 1's instead of 0's - for consecutive closers ) from the very right.
+              (generate [prev cumulated cons-closers num-of-closers]
+                ;(assert (and (<= 0 openers) (<= 0 closers) (<= 0 diff) (= diff (- closers openers))))
+                ;(assert (or (zero? diff) (pos? closers)))
+                ;(println "prev: " (clojure.pprint/cl-format nil "~,'0',B" prev))
+                ;(assert (= (count-digits prev true) (count-digits prev true) n-pairs) (str "prev: " prev))
+                
+                ; Openers and closers contain the respective bits, but:
+                ; - both use 1's - openers, and closers, too. (Closers will have them xor-ed later.)
+                ; - both contain bits, instead of number of bits (as was in parens-flat and parens-flatter).
+                ; - both are 0-based - closers, too (it will be shifted later).
+                ; Closers are the rightmost. Even though openers are left of closers, this symbol doesn't have them
+                ; shifted to the left. That's why we also produce num-of-closers, by which we later shift openers to the left.
+                (let [[swap-point openers closers num-of-closers]
+                      (loop [i num-of-closers
+                             openers 0
+                             closers cons-closers
+                             num-of-closers num-of-closers]
+                        ;(assert (= i (+ openers closers)))
+                        (if (bit-test prev i) #_an-opener?
+                          (let [openers+1 (inc openers)]
+                            (if (< openers+1 closers) #_swap-point?
+                              ;(list ...) is slower than [...], because CLJ optimises [...] with destructuring.
+                              [i openers closers] ;(list i openers closers)
+                              (if (= n-pairs*2-1 i) #_leftmost-digit?
+                                [0 0 0] ;(list 0 0 0) #_we-have-finished
+                                (recur   (inc i)      openers+1    closers))) #_an-opener-but-not-a-swap-point-yet)
+                          (recur         (inc i)      openers (inc closers)) #_a-closer))]
+                  
+                  (if (zero? swap-point)
+                    cumulated #_finished
+                    (let [;_ (assert (bit-test prev swap-point)) #_opener
+                          closers-1 (dec closers)
+                          value (loop [value (bit-flip prev swap-point) #_opener==>closer
+                                       i (dec swap-point) #_>>
+                                       openers (inc openers)
+                                       closers closers-1]
+                                  ;(println "value in loop:" (clojure.pprint/cl-format nil "~,'0',B" value) "openers:" openers "closers:" closers)
+                                  (cond
+                                    (pos? openers) (recur (bit-set   value i) (dec i) (dec openers)     closers)
+                                    (pos? closers) (recur (bit-clear value i) (dec i)      openers (dec closers))
+                                    :else  (do
+                                             ;(assert (neg? i))
+                                             value)))]
+                      ;(assert (number? value) (str "value from loop: " value))
+                      (recur value (cons value cumulated) closers-1)))))
+              (humanise [number]
+                (clojure.string/replace
+                  (clojure.string/replace (java.lang.Long/toBinaryString number) ;toBinaryString is faster 40ms than clojure.pprint/cl-format 308ms.
+                    \1 \()
+                  \0 \)))
+              ; Shift the value by 1 bit. Then set (or not) the lowest bit.
+              ; Use with to set first n-pairs bits as openers (, then clear next n-pairs bits as closers )
+              ; Not efficient, but it's only for starter. Return a pair (new-value bit) to be used with (iterate....). 
+              (shift-and-set [[value do-set]]
+                ;{:pre [(number? value) (or (= true do-set) (= false do-set)) #_no-boolean?-in-CLJ-1_4]}
+                (let [shifted (bit-shift-left value 1)
+                      new-value (if do-set
+                                  (bit-set shifted 0)
+                                  shifted)]
+                  (list new-value do-set)))]
+        (into #{}
+          (if (zero? n-pairs)
+            ()
+            (map humanise
+              (let [;n-pairs-1 (dec n-pairs)
+                    ; TODO the following runs indefinitely - but only with dbgf
+                    ;starter-ones (first (nth ;/---- that "dbgf" causes a runaway. Maybe because iterate is static:?
+                    ;                     (dbgf "iterate" iterate #(dbgf shift-and-set %) [0 true]) n-pairs))
+                    starter-ones (first (nth
+                                          (iterate #(shift-and-set %) [0 true]) n-pairs))
+                    ;_ (println "starter-ones" (clojure.pprint/cl-format nil "~,'0',B" starter-ones))
+                    starter      (first (nth  (iterate shift-and-set [starter-ones false]) n-pairs)) #_see-also-next-assert]
+                ;_ (println "starter" (clojure.pprint/cl-format nil "~,'0',B" starter))
+                ;_ (assert (= starter (bit-shift-left starter-ones n-pairs)))
+                ;_ (assert (= (digits starter) (concat (repeat n-pairs true) (repeat n-pairs false))))]
+                
+                (generate starter (list starter) n-pairs)))))))))
+
+(if (resolve 'time-check) ;from shell: clj -e "(def time-check true)" src/clojurist/
+  (println "time for n=12 (which generates " (time (count (parens-flatish 12))) "results"))
+
+;-----------
+; OLD, uneffective optimisation:
+; parens-flatter is like parens-flat, but it applies rightmost consecutive closers & openers into separate loop. That eliminates a complex conditional.
+; However, it's much slower: 390-400ms from shell -> 520-550ms!
 (def parens-flatter
   ; - prev: the last result
   ; binary 1 for an opener (, 0 for a closer )
@@ -69,7 +174,6 @@
   ; because the results are already unique.
   ; For n-pairs=12: 620-740ms. (map humanise ...) takes only 40-120ms out of the total.
   ; Added cons-closers => total 480ms. Turned off one last assert => total 460ms.
-  ; 390-400ms from shell
   (fn [n-pairs]
     (let [n-pairs*2 (* 2 n-pairs)
           n-pairs*2-1 (dec n-pairs*2)]
@@ -167,8 +271,6 @@
                 ;_ (assert (= (digits starter) (concat (repeat n-pairs true) (repeat n-pairs false))))]
                 
                 (generate starter (list starter) n-pairs)))))))))
-(if (resolve 'time-check) ;from shell: clj -e "(def time-check true)" src/clojurist/
-  (println "time for n=12 (which generates " (time (count (parens-flatter 12))) "results"))
 ;------
 (def parens-flat
   ; - prev: the last result
