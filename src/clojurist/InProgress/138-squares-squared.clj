c9d139abb28b0975d6d06d24e4583f12959c9562
Peter Kehl <peter.kehl@gmail.com>
2018-11-11 13:59:29 -0800

    PRogress

--- src/clojurist/InProgress/138-squares-squared.clj
+++ src/clojurist/InProgress/138-squares-squared.clj
@@ -36,10 +36,11 @@
                             [place-prev-right-neighbour dir-right]))
         new-row sorted-map ;sorted only for debugging, in production it can be hash-map
         entry? #(or (and (char? %) (<= 48 (int %) 57)) (= % \*))
-        fill-in (fn [places [x y :as place] value] {:pre [(matrix? places) (place? place) (entry? value)]} ;like assoc-in, but using new-map
+        fill-in (fn [places [x y :as place] value] {:pre [(matrix? places) (place? place) (entry? value)] :post [(= ((% x)y) value)]} ;like assoc-in, but using new-map
                   (let [row (get places x (new-row))
                         row-updated (assoc row y value)]
-                    (assoc places x row)))
+                    (println :fill-in :place place :value value)
+                    (assoc places x row-updated)))
         ;start with direction up to the right u-r, because place-and-dir will turn it to d-r after the 1st digit.
         ;To complete a square, we need an even number of turns (ignoring the very first "turn" from starting direction u-r).
         places (let [[places-for-digits place-last-digit dir-last-digit num-of-turns]
@@ -48,6 +49,8 @@
                             dir-last u-r
                             num-of-turns -1
                             digits-leftover (next digits)]
+                       (println :places) (println places)
+                       (println :place-last place-last :dir-last dir-last :num-of-turns num-of-turns :digits-leftover digits-leftover)
                        (if digits-leftover
                            (let [[place dir] (place-dir place-last dir-last)
                                  same-dir? (= dir-last dir)
@@ -58,13 +61,15 @@
                            [places place-last dir-last num-of-turns])
                        )
                      _ 1 ;when filling up with stars *, after the very last (even numbered) turn fill only places that have a neihbour on the right.
-                     _ (if (< 1 (count digits))
+                     _ (if false #_TODO-based-on-num-of-turns
                            1 ;fill up with stars
-                           places-for-digits)])
-        _ (do (println :places)(clojure.pprint/print-table places))
+                           places-for-digits)]
+                 places-for-digits)
+        _ (println :places places)
         rows-strings (let [[min-x max-x min-y max-y] (reduce (fn [[min-x max-x min-y max-y] [x y]]
                                                                [(min min-x x) (max max-x x) (min min-y y) (max max-y y)])
-                                                             [10 -10 10 -10] places)
+                                                             [10 -10 10 -10]
+                                                             (for [[x row] places, [y _] row] [x y]))
                            _ (println "min-x" min-x "max-x" max-x "min-y" min-y "max-y" max-y)
                            rows-with-spaces (for [x (range min-x (inc max-x))]
                                                (apply str
