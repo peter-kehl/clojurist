245e5bd27c38607eeb60b3fb92e4b3710d319eed
Peter Kehl <peter.kehl@gmail.com>
2018-11-12 12:24:06 -0800

    Moved

--- src/clojurist/4Clojure02.clj
+++ src/clojurist/4Clojure02.clj
@@ -271,50 +271,6 @@ assert-expr
 ; "form" means a [partial] expression
 ;http://www.4clojure.com/problem/72 (apply + seq) may be more efficient than (reduce + seq)
 
-;http://www.4clojure.com/problem/73 Tic Tac Toe
-(def tic
-  (fn [[row0 row1 row2]]
-      (let [won
-            (fn [[a b c]]
-              (or
-                  (and
-                      (= a b c)
-                      (some #{a} [:x :o])
-                      a) ;'a' makes (and) return value of a on succes (instead of boolean)
-                  nil))] ; 
-        
-        (or
-            (won row0) (won row1) (won row2)
-            (let [column
-                  (fn [col]
-                    [(row0 col) (row1 col) (row2 col)])]
-              (or
-                  (won (column 0)) (won (column 1)) (won (column 2))
-                  (won [(row0 0) (row1 1) (row2 2)])
-                  (won [(row0 2) (row1 1) (row2 0)])))))))
-(tic [[:e :e :e]
-      [:e :e :e]
-      [:e :e :e]])
-(tic [[:x :e :o]
-      [:x :e :e]
-      [:x :e :o]])
-(tic [[:x :e :o]
-      [:x :x :e]
-      [:o :x :o]])
-;others
-(fn [d]
-  (let [n [0 1 2]
-        s (set
-            (concat
-              d
-              (apply map list d)
-              [(for [a n] (get-in d [a a]))
-               (for [a n] (get-in d [(- 2 a) a]))]))]
-    (cond
-      (s [:x :x :x]) :x
-      (s [:o :o :o]) :o
-      :e nil)))
-;and many others
 
 ;http://www.4clojure.com/problem/74 filter perfect squares
 ((fn [str]
